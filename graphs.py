03308329a3df156999326e054b19248cff23b70d
Adam Zawierucha <zawiedev@gmail.com>
2020-06-26 14:05:43 -0700

    create initial graph generator

---
+++ graphs.py
@@ -0,0 +1,32 @@
+import random
+import matrix
+
+def randomGraph(nodeCount,weighted=False,directed=False):
+    """
+    Generates and returns a random adjaceny matrix
+    """
+    adjacenyMatrix = matrix.BlankSquareMatrix(nodeCount)
+    for y in range(nodeCount):
+        for x in range(0 if directed else y,nodeCount):
+            if x == y:
+                #Identical nodes have similiart of 1
+                adjacenyMatrix[x][y] = 1
+            else:
+                #Generate Random Edge weight
+                adjacenyMatrix[y][x] = random.random() if weighted else round(random.random())
+                if directed == False:
+                    adjacenyMatrix[x][y] = adjacenyMatrix[y][x]
+    return adjacenyMatrix
+
+def readGraph(fileName):
+    """
+    Returns the adajency matrix of a given graph stored at data/file_name
+    """
+    pass
+
+def KarateClub():
+    return readGraph("KaraClub")
+def SocialNetwork():
+    return readGraph("SocialNetwork")
+
+matrix.printMat(randomGraph(10))
